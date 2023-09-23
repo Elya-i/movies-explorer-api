635c3f6e94061d569167437b0ea9d25fe0f6e09b
ElyA_I <elyaizmaylova@gmail.com>
2023-09-23 17:41:25 +0200

    feat: create API schemas and resource models

---
+++ models/user.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const isEmail = require('validator/lib/isEmail');
+const UnauthorizedError = require('../errors/UnauthorizedError');
+const {
+  INCORRECT_EMAIL_MESSAGE,
+  INCORRECT_AUTHORIZATION_DATA_MESSAGE,
+} = require('../utils/constants');
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: [true, 'Поле "email" должно быть заполнено'],
+    validate: {
+      validator: (v) => isEmail(v),
+      message: INCORRECT_EMAIL_MESSAGE,
+    },
+  },
+  password: {
+    type: String,
+    required: [true, 'Поле "password" должно быть заполнено'],
+    select: false,
+  },
+  name: {
+    type: String,
+    required: [true, 'Поле "name" должно быть заполнено'],
+    minlength: [2, 'Минимальная длина поля "name" - 2 символа'],
+    maxlength: [30, 'Максимальная длина поля "name" - 30 символов'],
+  },
+}, {
+  versionKey: false,
+
+  statics: {
+    findUserByCredentials(email, password) {
+      return this.findOne({ email }).select('+password')
+        .then((user) => {
+          if (!user) {
+            return Promise.reject(new UnauthorizedError(INCORRECT_AUTHORIZATION_DATA_MESSAGE));
+          }
+          return bcrypt.compare(password, user.password)
+            .then((matched) => {
+              if (!matched) {
+                return Promise.reject(new UnauthorizedError(INCORRECT_AUTHORIZATION_DATA_MESSAGE));
+              }
+              return user;
+            });
+        });
+    },
+  },
+});
+
+module.exports = mongoose.model('user', userSchema);
---
+++ utils/constants.js
@@ -0,0 +1,29 @@
+const DEFAULT_ERROR_MESSAGE = 'На сервере произошла ошибка!';
+const PAGE_NOT_FOUND_MESSAGE = 'Запрашиваемый ресурc не найден';
+const INCORRECT_AUTHORIZATION_DATA_MESSAGE = 'Неправильные почта или пароль';
+const CONFLICT_MESSAGE = 'Пользователь с указанным email уже зарегистрирован';
+const FORBIDDEN_MESSAGE = 'Отсутствуют права доступа на удаление фильма';
+const AUTHORIZATION_SUCCES_MESSAGE = 'Авторизация прошла успешно';
+const LOGOUT_SUCCES_MESSAGE = 'Успешный выход';
+const INCORRECT_USER_ID_MESSAGE = 'Передан некорректный _id пользователя';
+const INCORRECT_USER_DATA_MESSAGE = 'Переданы некорректные данные при обновлении профиля';
+const USER_NOT_FOUND_MESSAGE = 'Пользователь по указанному _id не найден';
+const LIMITER_MESSAGE = 'Too many requests';
+const INCORRECT_EMAIL_MESSAGE = 'Неправильный формат email!';
+const INCORRECT_URL_MESSAGE = 'Неправильный формат url!';
+
+module.exports = {
+  DEFAULT_ERROR_MESSAGE,
+  PAGE_NOT_FOUND_MESSAGE,
+  INCORRECT_AUTHORIZATION_DATA_MESSAGE,
+  CONFLICT_MESSAGE,
+  FORBIDDEN_MESSAGE,
+  AUTHORIZATION_SUCCES_MESSAGE,
+  LOGOUT_SUCCES_MESSAGE,
+  INCORRECT_USER_ID_MESSAGE,
+  INCORRECT_USER_DATA_MESSAGE,
+  USER_NOT_FOUND_MESSAGE,
+  LIMITER_MESSAGE,
+  INCORRECT_EMAIL_MESSAGE,
+  INCORRECT_URL_MESSAGE,
+};
